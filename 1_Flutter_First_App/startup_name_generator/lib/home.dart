35ccc8620e7ab7b58197a02a4857de447c6fde70
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-07-23 19:39:18 +0300

    Section1 is completed

--- 1_Flutter_First_App/startup_name_generator/lib/home.dart
+++ 1_Flutter_First_App/startup_name_generator/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,13 +7,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final List<WordPair> _suggestions = <WordPair>[];            // NEW
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18); // NEW
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Startup Name Generator"),
       ),
-      body: Center(child: Text("Hello World"),),
+      body: _buildSuggestions(),
     );
   }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+   Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
 }
